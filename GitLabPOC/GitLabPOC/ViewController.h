70b7ca52d4bb296b39ceaf37f6d9c1d2c6efd7a1
ambujshukla <ambuj.shukla99@gmail.com>
2016-08-30 11:56:28 +0530

    Adding FreshCode
    Adding code to repo for first time.

---
+++ GitLabPOC/GitLabPOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitLabPOC
+//
+//  Created by cdn68 on 30/08/16.
+//  Copyright © 2016 cdn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
